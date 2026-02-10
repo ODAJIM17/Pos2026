f04482377517424e079e2a0a2619686ff3ef73c3
ODAJIM17 <Davila56jd@gmail.com>
2026-02-10 16:53:27 -0500

    Project Setup

---
+++ POS/POS.Shared/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace POS.Shared.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Category")]
+        [MaxLength(30, ErrorMessage = "Field {0} can not exceed {1} characters")]
+        [Required(ErrorMessage = "{0} is required.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Description")]
+        [MaxLength(50, ErrorMessage = "Field {0} can not exceed {1} characters")]
+        [Required(ErrorMessage = "{0} is required.")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
